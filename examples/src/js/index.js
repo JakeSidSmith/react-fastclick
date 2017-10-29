23f63d365868c773f90a24df375bb950dac793e7
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-29 14:55:18 +0000

    A little style

--- examples/src/js/index.js
+++ examples/src/js/index.js
@@ -92,17 +92,19 @@ const Topics = ({ match }) => (
 const App = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+      </nav>
 
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <main>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </main>
     </div>
   </Router>
 );
